4cae4a99f5c73d48e02cab3879ed56f7e1e45c9f
hikida <ponponda0103@gmail.com>
2025-02-02 02:08:24 +0900

    practice

--- src/main/java/com/example/GCS/controller/HelloController.java
+++ src/main/java/com/example/GCS/controller/HelloController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000") // React のURL
+
+@CrossOrigin(origins = "https://githubcontributesystem.netlify.app") // React のURL
 @RestController
 public class HelloController {
 
