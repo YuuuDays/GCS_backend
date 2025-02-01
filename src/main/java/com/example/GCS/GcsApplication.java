87be7fb49b9b535c6e6e2d7934bfd254ed4fc99c
hikida <ponponda0103@gmail.com>
2025-02-01 20:11:08 +0900

    practice

---
+++ src/main/java/com/example/GCS/GcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.GCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GCS/GcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
